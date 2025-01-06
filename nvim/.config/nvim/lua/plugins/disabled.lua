2b0086aa0fe023980c9025dacca8c2d9ce4002ed
Oleksandr Podze <sasha.podze@people.ai>
2025-01-06 14:08:03 +0100

    disable smooth scrolling

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "snacks.nvim", opts = { scroll = { enabled = false } } },
   { "echasnovski/mini.pairs", enabled = false },
 }
