b9e0f9409c7c9b2c45e9953777e3c85721d12e73
Oleksandr Podze <dasdeg@gmail.com>
2025-05-13 19:59:42 +0200

    arch mirrors ranging function

--- needed-arch-commands.sh
+++ needed-arch-commands.sh
@@ -8,11 +8,19 @@ setup_paru() {
     makepkg -si
 }
 
-rustup default stable
+rustup-setup() {
+    rustup default stable
+}
 
 call_chris_titus_util() {
     curl -fsSL https://christitus.com/linux | sh
 }
+
+arch_mirrors() {
+    url="https://archlinux.org/mirrorlist/?country=CZ&country=DE&protocol=https&use_mirror_status=on"
+    curl -s $url | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 10 -
+}
+
 
 tldr_setup() {
     sudo pacman -S --needed tealdeer
