2ec3425b515c23aa011a539e196228c387894781
Oleksandr Podze <dasdeg@gmail.com>
2025-05-22 00:12:39 +0200

    add buffer-agnostic keymaps; C does not overwrite registers ever

--- nvim/dot-config/nvim/lua/config/keymaps.lua
+++ nvim/dot-config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
 vim.keymap.set({ "n", "v" }, "x", '"_x')
+vim.keymap.set({ "v", "n" }, "c", '"_c')
+vim.keymap.set({ "v", "n" }, "C", '"_C')
 vim.keymap.set("n", "<leader>d", '"_d', { desc = "Delete without changing clipboard" })
 vim.keymap.set("v", "<leader>dd", '"_d', { desc = "Delete without changing clipboard" })
 vim.keymap.set("n", "<leader>h", ":noh<CR>", { desc = "Stop highlighting local search" })
