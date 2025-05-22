e92ca1dec357363753b3183a8fbedc0627cb896e
Oleksandr Podze <dasdeg@gmail.com>
2025-05-22 20:03:52 +0200

    add line moving (not simple keybinds bc I see the bar blinking)

--- nvim/dot-config/nvim/lua/config/lazy.lua
+++ nvim/dot-config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
+    { import = "lazyvim.plugins.extras.editor.mini-move" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
