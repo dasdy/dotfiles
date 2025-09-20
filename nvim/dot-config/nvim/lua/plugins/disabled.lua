0b523e628b2cbdc8603345eea4ee485eb663ea4d
Oleksandr Podze <dasdeg@gmail.com>
2025-09-20 17:46:07 +0200

    update renamed plugins' names

--- nvim/dot-config/nvim/lua/plugins/disabled.lua
+++ nvim/dot-config/nvim/lua/plugins/disabled.lua
@@ -2,5 +2,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "snacks.nvim", opts = { scroll = { enabled = false } } },
-  { "echasnovski/mini.pairs", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
 }
