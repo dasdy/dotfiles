0c87ad262fde2f1d5c46cce277eceac7dbaba2f1
Oleksandr Podze <dasdeg@gmail.com>
2025-10-18 13:57:35 +0200

    disable lazyvim module in line and startup notifications

--- nvim/dot-config/nvim/lua/config/lazy.lua
+++ nvim/dot-config/nvim/lua/config/lazy.lua
@@ -53,7 +53,8 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "catppuccin" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  -- automatically check for plugin updates. avoid nagging with notifications about this though.
+  checker = { enabled = true, notify = false },
   performance = {
     rtp = {
       -- disable some rtp plugins
--- nvim/dot-config/nvim/lua/plugins/lualine.lua
+++ nvim/dot-config/nvim/lua/plugins/lualine.lua
@@ -4,6 +4,7 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     sections = {
+
       lualine_c = {
         LazyVim.lualine.root_dir(),
         {
@@ -18,13 +19,52 @@ return {
         -- Remove the file icon
         { LazyVim.lualine.pretty_path() },
       },
-      lualine_y = {
-        { "progress", separator = " ", padding = { left = 1, right = 0 } },
-        { "location", padding = { left = 0, right = 1 } },
+
+      lualine_x = {
+        -- main change from lazyvim: remove update notification thing.
+        -- on the way, also removed the "snacks profile segment" thing because I don't understand
+        -- snacks.lua I guess.
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.command.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+          color = function() return { fg = Snacks.util.color("Statement") } end,
+        },
+        -- stylua: ignore
+        {
+            function() return require("noice").api.status.mode.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+            color = function() return { fg = Snacks.util.color("Constant") } end,
+        },
+        -- stylua: ignore
+        {
+          function() return "  " .. require("dap").status() end,
+          cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+          color = function() return { fg = Snacks.util.color("Debug") } end,
+        },
+        {
+          "diff",
+          symbols = {
+            added = icons.git.added,
+            modified = icons.git.modified,
+            removed = icons.git.removed,
+          },
+          source = function()
+            local gitsigns = vim.b.gitsigns_status_dict
+            if gitsigns then
+              return {
+                added = gitsigns.added,
+                modified = gitsigns.changed,
+                removed = gitsigns.removed,
+              }
+            end
+          end,
+        },
       },
+      -- lualine_y (right middle module) is the same
       -- Replace time with file type
       lualine_z = { "filetype" },
     },
-    extensions = { "neo-tree", "lazy" },
+    extensions = { "neo-tree", "fzf" },
   },
 }
