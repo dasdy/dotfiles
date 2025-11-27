5b9d67bb4aae3776df48fe35c7504959a8828daf
Oleksandr Podze <dasdeg@gmail.com>
2025-11-27 22:39:14 +0100

    experiment: enable flash nvim

--- nvim/dot-config/nvim/lua/plugins/disabled.lua
+++ nvim/dot-config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
   { "snacks.nvim", opts = { scroll = { enabled = false } } },
   { "nvim-mini/mini.pairs", enabled = false },
 }
