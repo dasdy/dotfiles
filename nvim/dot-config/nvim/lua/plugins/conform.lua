32e2d95330f8188619a4864fff3e03c68b3bb267
Oleksandr Podze <dasdeg@gmail.com>
2026-02-10 15:39:26 +0100

    ruff format by default, guide on how to change that

--- nvim/dot-config/nvim/lua/plugins/conform.lua
+++ nvim/dot-config/nvim/lua/plugins/conform.lua
@@ -5,6 +5,8 @@ return {
       formatters_by_ft = {
         -- disable html formatter - it conflicts with templ
         templ = { "templ" },
+        python = { "ruff_format" },
+        -- python = { "black" }, -- comment above, uncomment this to change python formatter
       },
       default_format_opts = {
         timeout_ms = 10000,
